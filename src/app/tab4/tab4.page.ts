7247d59e6556a9586d5605320fca581b7567691e
Nico Gilardoni <nicogilardonik@gmail.com>
2025-01-30 19:54:56 -0300

    feat: :sparkles: add Tab 4 page with routing and Tailwind CSS integration

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -4,23 +4,21 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
-  IonTextarea,
-  IonButton,
 } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
   imports: [
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
-    IonTextarea,
-    IonButton,
+    ExploreContainerComponent,
   ],
 })
-export class Tab3Page {
+export class Tab4Page {
   constructor() {}
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,18 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import {
+  barbellOutline,
+  timeOutline,
+  chatboxEllipsesOutline,
+  personOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +24,11 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({
+      barbellOutline,
+      timeOutline,
+      chatboxEllipsesOutline,
+      personOutline,
+    });
   }
 }
