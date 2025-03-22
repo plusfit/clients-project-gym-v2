4e0e825cc1f82188a75333a831a418ad8bee1452
nahuelgilvangwe <nahuel@vangwe.com>
2025-03-22 10:26:21 -0300

    fix: :bug: correct typo in tab navigation link and add 'standalone' property to TabsPage component; enhance styling by adding h3 element with APEXPRO font

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -38,6 +38,7 @@ import {
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
+  standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
