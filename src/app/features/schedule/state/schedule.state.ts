2d69afb417638141c7b018099a697b6039784a86
nahuelgilvangwe <nahuel@vangwe.com>
2025-03-30 18:10:53 -0300

    feat: :sparkles: implement ScheduleFacadeService for managing schedules; refactor ScheduleService to use API for fetching schedules and user enrollments

--- src/app/features/schedule/state/schedule.state.ts
+++ src/app/features/schedule/state/schedule.state.ts
@@ -38,60 +38,7 @@ export class UnenrollFromSchedule {
 @State<ScheduleStateModel>({
   name: 'schedule',
   defaults: {
-    schedules: [
-      {
-        _id: '678bfbee653d4a1602d7adfe',
-        startTime: '7',
-        endTime: '8',
-        maxCount: 7,
-        clients: [
-          '6716d37ef04b1f954f0bbbfe',
-          '679a686ea392598c8f90ad24',
-          '679a6d63a3a8c508e7fa132f',
-        ],
-        day: 'Lunes',
-      },
-      {
-        _id: '678d3b3f4d6dc701e49cf035',
-        startTime: '6',
-        endTime: '7',
-        maxCount: 7,
-        clients: [],
-        day: 'Lunes',
-      },
-      {
-        _id: '6793c8c86b0c3d40ba56f8d6',
-        startTime: '6',
-        endTime: '7',
-        maxCount: 7,
-        clients: [],
-        day: 'Martes',
-      },
-      {
-        _id: '6793c8c86b0c3d40ba56f8d8',
-        startTime: '6',
-        endTime: '7',
-        maxCount: 7,
-        clients: [],
-        day: 'Miércoles',
-      },
-      {
-        _id: '6793c8c86b0c3d40ba56f8da',
-        startTime: '6',
-        endTime: '7',
-        maxCount: 7,
-        clients: [],
-        day: 'Jueves',
-      },
-      {
-        _id: '6793c8c86b0c3d40ba56f8dc',
-        startTime: '6',
-        endTime: '7',
-        maxCount: 7,
-        clients: [],
-        day: 'Viernes',
-      },
-    ],
+    schedules: [],
   },
 })
 export class ScheduleState {
