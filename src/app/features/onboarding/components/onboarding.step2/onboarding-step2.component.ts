cd8787f2bd266d8de80d85e20d2d7609b946e72e
Nico Gilardoni <nicogilardonik@gmail.com>
2025-03-31 16:54:56 -0300

    feat: :sparkles: implement onboarding step 2 and step 3 with form validation and styling improvements

--- src/app/features/onboarding/components/onboarding.step2/onboarding-step2.component.ts
+++ src/app/features/onboarding/components/onboarding.step2/onboarding-step2.component.ts
@@ -1,16 +1,60 @@
-import { Component } from '@angular/core';
-import { IonNav } from '@ionic/angular';
+import { Component, Input } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { IonicModule, IonNav } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 import { OnboardingStep3Component } from '../onboarding.step3/onboarding-step3.component';
 
 @Component({
   selector: 'app-onboarding-step2',
-  templateUrl: './onboarding-step2.component.html',
   standalone: true,
+  templateUrl: './onboarding-step2.component.html',
+  styleUrls: ['./onboarding-step2.component.scss'],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule],
 })
 export class OnboardingStep2Component {
-  constructor(private nav: IonNav) {}
+  @Input() nav!: IonNav;
+  @Input() userData: any;
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      bloodPressure: ['', Validators.required],
+      antecedentes: this.fb.group({
+        respiratorios: ['', Validators.required],
+        cardiacos: ['', Validators.required],
+        quirurgicos: ['', Validators.required],
+        lesiones: ['', Validators.required],
+      }),
+    });
+  }
 
   nextStep() {
-    this.nav.push(OnboardingStep3Component);
+    //   if (this.form.valid) {
+    const step2Data = this.form.value;
+
+    this.nav.push(OnboardingStep3Component, {
+      nav: this.nav,
+      userData: { ...this.userData, ...step2Data },
+    });
+    //    } else {
+    //      this.form.markAllAsTouched();
+    //    }
+  }
+
+  prevStep() {
+    this.nav.pop();
+  }
+
+  isInvalid(control: string, group: string = '') {
+    const c = group
+      ? (this.form.get(group) as FormGroup).get(control)
+      : this.form.get(control);
+    return c?.invalid && c?.touched;
   }
 }
