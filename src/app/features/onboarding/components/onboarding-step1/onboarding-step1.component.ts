9b222765c6be3ce2c36de5dd5427bcf07ad4c687
nahuelgilvangwe <nahuel@vangwe.com>
2025-04-05 17:57:21 -0300

    style: :art: enhance onboarding components with improved layout, icons, and validation messages for better user experience and visual consistency

--- src/app/features/onboarding/components/onboarding-step1/onboarding-step1.component.ts
+++ src/app/features/onboarding/components/onboarding-step1/onboarding-step1.component.ts
@@ -1,86 +1,108 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-  AbstractControl,
-  ValidationErrors,
-} from '@angular/forms';
-import { ModalController } from '@ionic/angular';
-import { IonicModule, IonNav } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { OnboardingStep2Component } from '../onboarding.step2/onboarding-step2.component';
-import { IonDatetimeModalComponent } from '@shared/components/IonDatetimeModal/ion-datetime-modal.component';
-import { Store } from '@ngxs/store';
-import { SetStep1 } from '../../state/onboarding.actions';
+	AbstractControl,
+	FormBuilder,
+	FormGroup,
+	ReactiveFormsModule,
+	ValidationErrors,
+	Validators,
+} from "@angular/forms";
+import { ModalController } from "@ionic/angular";
+import { IonNav, IonicModule } from "@ionic/angular";
+import { Store } from "@ngxs/store";
+import { IonDatetimeModalComponent } from "@shared/components/IonDatetimeModal/ion-datetime-modal.component";
+import { addIcons } from "ionicons";
+import {
+	arrowForward,
+	calendarOutline,
+	callOutline,
+	cardOutline,
+	homeOutline,
+	medkitOutline,
+	peopleOutline,
+	personOutline,
+} from "ionicons/icons";
+import { SetStep1 } from "../../state/onboarding.actions";
+import { OnboardingStep2Component } from "../onboarding.step2/onboarding-step2.component";
 
 @Component({
-  selector: 'app-step1',
-  standalone: true,
-  imports: [CommonModule, IonicModule, ReactiveFormsModule],
-  templateUrl: './onboarding-step1.component.html',
-  styleUrls: ['./onboarding-step1.component.scss'],
+	selector: "app-step1",
+	standalone: true,
+	imports: [CommonModule, IonicModule, ReactiveFormsModule],
+	templateUrl: "./onboarding-step1.component.html",
+	styleUrls: ["./onboarding-step1.component.scss"],
 })
 export class OnboardingStep1Component {
-  @Input() nav!: IonNav;
-  userForm: FormGroup;
+	@Input() nav!: IonNav;
+	userForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private modalCtrl: ModalController,
-    private store: Store,
-  ) {
-    this.userForm = this.fb.group({
-      fullName: ['', Validators.required],
-      address: ['', Validators.required],
-      phone: ['', [Validators.required, Validators.pattern(/^09\d{7}$/)]],
-      mutual: ['', Validators.required],
-      dateOfBirth: ['', Validators.required],
-      sex: ['', Validators.required],
-      ci: ['', [Validators.required, Validators.pattern(/^\d{8}$/)]],
-    });
-  }
+	constructor(
+		private fb: FormBuilder,
+		private modalCtrl: ModalController,
+		private store: Store,
+	) {
+		addIcons({
+			"person-outline": personOutline,
+			"home-outline": homeOutline,
+			"call-outline": callOutline,
+			"medkit-outline": medkitOutline,
+			"calendar-outline": calendarOutline,
+			"people-outline": peopleOutline,
+			"card-outline": cardOutline,
+			"arrow-forward": arrowForward,
+		});
 
-  setDateOfBirth(event: any) {
-    const date = event.detail.value;
-    this.userForm.get('dateOfBirth')?.setValue(date);
-  }
-  //BORRO PARA IR MAS RAPIDO
-  nextStep() {
-    if (this.userForm.valid) {
-      const step1Data = this.userForm.value;
-      this.store.dispatch(new SetStep1(step1Data));
-      this.nav.push(OnboardingStep2Component, { step1Data, nav: this.nav });
-    } else {
-      this.userForm.markAllAsTouched();
-    }
-  }
+		this.userForm = this.fb.group({
+			fullName: ["", Validators.required],
+			address: ["", Validators.required],
+			phone: ["", [Validators.required, Validators.pattern(/^09\d{7}$/)]],
+			mutual: ["", Validators.required],
+			dateOfBirth: ["", Validators.required],
+			sex: ["", Validators.required],
+			ci: ["", [Validators.required, Validators.pattern(/^\d{8}$/)]],
+		});
+	}
 
-  isInvalid(controlName: string): boolean {
-    const control = this.userForm.get(controlName);
-    return (control?.invalid && control?.touched) || false;
-  }
+	setDateOfBirth(event: any) {
+		const date = event.detail.value;
+		this.userForm.get("dateOfBirth")?.setValue(date);
+	}
+	//BORRO PARA IR MAS RAPIDO
+	nextStep() {
+		if (this.userForm.valid) {
+			const step1Data = this.userForm.value;
+			this.store.dispatch(new SetStep1(step1Data));
+			this.nav.push(OnboardingStep2Component, { step1Data, nav: this.nav });
+		} else {
+			this.userForm.markAllAsTouched();
+		}
+	}
 
-  async openBirthdateModal() {
-    const today = new Date().toISOString().split('T')[0]; //yyyy-mm-dd
-    const modal = await this.modalCtrl.create({
-      component: IonDatetimeModalComponent,
-      breakpoints: [0, 0.4],
-      initialBreakpoint: 0.6,
-      componentProps: {
-        value: this.userForm.get('dateOfBirth')?.value,
-        max: today,
-      },
-      cssClass: 'datepicker-sheet',
-    });
+	isInvalid(controlName: string): boolean {
+		const control = this.userForm.get(controlName);
+		return (control?.invalid && control?.touched) || false;
+	}
 
-    modal.onWillDismiss().then((data) => {
-      const selected = data.data;
-      if (selected) {
-        this.userForm.get('dateOfBirth')?.setValue(selected);
-      }
-    });
-    await modal.present();
-  }
+	async openBirthdateModal() {
+		const today = new Date().toISOString().split("T")[0]; //yyyy-mm-dd
+		const modal = await this.modalCtrl.create({
+			component: IonDatetimeModalComponent,
+			breakpoints: [0, 0.4],
+			initialBreakpoint: 0.6,
+			componentProps: {
+				value: this.userForm.get("dateOfBirth")?.value,
+				max: today,
+			},
+			cssClass: "datepicker-sheet",
+		});
+
+		modal.onWillDismiss().then((data) => {
+			const selected = data.data;
+			if (selected) {
+				this.userForm.get("dateOfBirth")?.setValue(selected);
+			}
+		});
+		await modal.present();
+	}
 }
--- src/app/features/onboarding/components/onboarding.step2/onboarding-step2.component.ts
+++ src/app/features/onboarding/components/onboarding.step2/onboarding-step2.component.ts
@@ -1,67 +1,71 @@
-import { Component, Input } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { IonicModule, IonNav } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { OnboardingStep3Component } from '../onboarding.step3/onboarding-step3.component';
-import { Store } from '@ngxs/store';
-import { SetStep2 } from '../../state/onboarding.actions';
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { IonNav, IonicModule } from "@ionic/angular";
+import { Store } from "@ngxs/store";
+import { addIcons } from "ionicons";
+import { arrowBack, arrowForward, bandageOutline, fitnessOutline, heartOutline, pulseOutline } from "ionicons/icons";
+import { SetStep2 } from "../../state/onboarding.actions";
+import { OnboardingStep3Component } from "../onboarding.step3/onboarding-step3.component";
 
 @Component({
-  selector: 'app-onboarding-step2',
-  standalone: true,
-  templateUrl: './onboarding-step2.component.html',
-  styleUrls: ['./onboarding-step2.component.scss'],
-  imports: [CommonModule, IonicModule, ReactiveFormsModule],
+	selector: "app-onboarding-step2",
+	standalone: true,
+	templateUrl: "./onboarding-step2.component.html",
+	styleUrls: ["./onboarding-step2.component.scss"],
+	imports: [CommonModule, IonicModule, ReactiveFormsModule],
 })
 export class OnboardingStep2Component {
-  @Input() nav!: IonNav;
-  @Input() userData: any;
+	@Input() nav!: IonNav;
+	@Input() userData: any;
 
-  form: FormGroup;
+	form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private store: Store,
-  ) {
-    this.form = this.fb.group({
-      bloodPressure: ['', Validators.required],
-      history: this.fb.group({
-        respiratory: ['', Validators.required],
-        cardiac: ['', Validators.required],
-        chirurgical: ['', Validators.required],
-        injuries: ['', Validators.required],
-      }),
-    });
-  }
+	constructor(
+		private fb: FormBuilder,
+		private store: Store,
+	) {
+		addIcons({
+			"pulse-outline": pulseOutline,
+			"fitness-outline": fitnessOutline,
+			"heart-outline": heartOutline,
+			"bandage-outline": bandageOutline,
+			"arrow-forward": arrowForward,
+			"arrow-back": arrowBack,
+		});
 
-  nextStep() {
-    if (this.form.valid) {
-      const step2Data = this.form.value;
+		this.form = this.fb.group({
+			bloodPressure: ["", Validators.required],
+			history: this.fb.group({
+				respiratory: ["", Validators.required],
+				cardiac: ["", Validators.required],
+				chirurgical: ["", Validators.required],
+				injuries: ["", Validators.required],
+			}),
+		});
+	}
 
-      this.store.dispatch(new SetStep2(step2Data));
+	nextStep() {
+		if (this.form.valid) {
+			const step2Data = this.form.value;
 
-      this.nav.push(OnboardingStep3Component, {
-        nav: this.nav,
-        userData: { ...this.userData, ...step2Data },
-      });
-    } else {
-      this.form.markAllAsTouched();
-    }
-  }
+			this.store.dispatch(new SetStep2(step2Data));
 
-  prevStep() {
-    this.nav.pop();
-  }
+			this.nav.push(OnboardingStep3Component, {
+				nav: this.nav,
+				userData: { ...this.userData, ...step2Data },
+			});
+		} else {
+			this.form.markAllAsTouched();
+		}
+	}
 
-  isInvalid(control: string, group: string = '') {
-    const c = group
-      ? (this.form.get(group) as FormGroup).get(control)
-      : this.form.get(control);
-    return c?.invalid && c?.touched;
-  }
+	prevStep() {
+		this.nav.pop();
+	}
+
+	isInvalid(control: string, group = "") {
+		const c = group ? (this.form.get(group) as FormGroup).get(control) : this.form.get(control);
+		return c?.invalid && c?.touched;
+	}
 }
