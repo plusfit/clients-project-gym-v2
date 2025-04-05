f2d2b9ee676b4db69eb145e57633f707cfaabdd3
nahuelgilvangwe <nahuel@vangwe.com>
2025-04-05 17:57:21 -0300

    style: :art: enhance onboarding components with improved layout, icons, and validation messages for better user experience and visual consistency

--- src/app/features/onboarding/components/onboarding.step3/onboarding-step3.component.ts
+++ src/app/features/onboarding/components/onboarding.step3/onboarding-step3.component.ts
@@ -1,57 +1,68 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { IonNav, IonicModule } from "@ionic/angular";
+import { Store } from "@ngxs/store";
+import { addIcons } from "ionicons";
 import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { IonicModule, IonNav } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { Store } from '@ngxs/store';
-import { SetStep3 } from '../../state/onboarding.actions';
+	arrowBack,
+	barChartOutline,
+	barbellOutline,
+	calendarOutline,
+	checkmarkOutline,
+	trendingUpOutline,
+} from "ionicons/icons";
+import { SetStep3 } from "../../state/onboarding.actions";
 
 @Component({
-  selector: 'app-onboarding-step3',
-  standalone: true,
-  templateUrl: './onboarding-step3.component.html',
-  styleUrls: ['./onboarding-step3.component.scss'],
-  imports: [CommonModule, IonicModule, ReactiveFormsModule],
+	selector: "app-onboarding-step3",
+	standalone: true,
+	templateUrl: "./onboarding-step3.component.html",
+	styleUrls: ["./onboarding-step3.component.scss"],
+	imports: [CommonModule, IonicModule, ReactiveFormsModule],
 })
 export class OnboardingStep3Component {
-  @Input() nav!: IonNav;
-  @Input() userData: any;
+	@Input() nav!: IonNav;
+	@Input() userData: any;
 
-  form: FormGroup;
+	form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private store: Store,
-  ) {
-    this.form = this.fb.group({
-      trainingDays: [3, Validators.required],
-      goal: ['', Validators.required],
-      trainingType: ['', Validators.required],
-      trainingLevel: ['', Validators.required],
-    });
-  }
+	constructor(
+		private fb: FormBuilder,
+		private store: Store,
+	) {
+		addIcons({
+			"calendar-outline": calendarOutline,
+			"trending-up-outline": trendingUpOutline,
+			"barbell-outline": barbellOutline,
+			"bar-chart-outline": barChartOutline,
+			"arrow-back": arrowBack,
+			"checkmark-outline": checkmarkOutline,
+		});
 
-  nextStep() {
-    if (this.form.valid) {
-      const step3Data = this.form.value;
-      this.store.dispatch(new SetStep3(step3Data));
-    } else {
-      this.form.markAllAsTouched();
-    }
-  }
+		this.form = this.fb.group({
+			trainingDays: [3, Validators.required],
+			goal: ["", Validators.required],
+			trainingType: ["", Validators.required],
+			trainingLevel: ["", Validators.required],
+		});
+	}
 
-  prevStep() {
-    this.nav.pop();
-  }
+	nextStep() {
+		if (this.form.valid) {
+			const step3Data = this.form.value;
+			this.store.dispatch(new SetStep3(step3Data));
+		} else {
+			this.form.markAllAsTouched();
+		}
+	}
 
-  isInvalid(control: string, group: string = '') {
-    const c = group
-      ? (this.form.get(group) as FormGroup).get(control)
-      : this.form.get(control);
-    return c?.invalid && c?.touched;
-  }
+	prevStep() {
+		this.nav.pop();
+	}
+
+	isInvalid(control: string, group = "") {
+		const c = group ? (this.form.get(group) as FormGroup).get(control) : this.form.get(control);
+		return c?.invalid && c?.touched;
+	}
 }
