321660c7551a400d0fadd34d241a5c1ba2a45545
Nico Gilardoni <nicogilardonik@gmail.com>
2025-04-16 09:02:20 -0300

    feat: :sparkles: enhance register form component with improved state management and success notifications

--- src/app/features/auth/components/register-form/register-form.component.ts
+++ src/app/features/auth/components/register-form/register-form.component.ts
@@ -1,88 +1,104 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  AbstractControl,
-  ValidationErrors,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+	AbstractControl,
+	FormBuilder,
+	FormGroup,
+	ReactiveFormsModule,
+	ValidationErrors,
+	Validators,
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { Register } from "@feature/auth/state/auth.actions";
+import { IonicModule } from "@ionic/angular";
+import { Actions, Store, ofActionSuccessful } from "@ngxs/store";
+import { ToastService } from "@shared/services/toast.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
-  selector: 'app-register-form',
-  standalone: true,
-  templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss'],
-  imports: [CommonModule, IonicModule, ReactiveFormsModule],
+	selector: "app-register-form",
+	standalone: true,
+	templateUrl: "./register-form.component.html",
+	styleUrls: ["./register-form.component.scss"],
+	imports: [CommonModule, IonicModule, ReactiveFormsModule],
 })
 export class RegisterFormComponent {
-  form: FormGroup;
+	form: FormGroup;
+	private _destroyed = new Subject<void>();
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-  ) {
-    this.form = this.fb.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        repeatEmail: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, this.passwordValidator]],
-        repeatPassword: ['', Validators.required],
-      },
-      {
-        validators: [this.emailsMatchValidator, this.passwordsMatchValidator],
-      },
-    );
-  }
+	constructor(
+		private fb: FormBuilder,
+		private router: Router,
+		private store: Store,
+		private actions: Actions,
+		private toastService: ToastService,
+	) {
+		this.form = this.fb.group(
+			{
+				email: ["", [Validators.required, Validators.email]],
+				repeatEmail: ["", [Validators.required, Validators.email]],
+				password: ["", [Validators.required, this.passwordValidator]],
+				repeatPassword: ["", Validators.required],
+			},
+			{
+				validators: [this.emailsMatchValidator, this.passwordsMatchValidator],
+			},
+		);
+	}
 
-  emailsMatchValidator(group: AbstractControl): ValidationErrors | null {
-    const email = group.get('email')?.value;
-    const repeat = group.get('repeatEmail')?.value;
-    return email === repeat ? null : { emailsDontMatch: true };
-  }
+	emailsMatchValidator(group: AbstractControl): ValidationErrors | null {
+		const email = group.get("email")?.value;
+		const repeat = group.get("repeatEmail")?.value;
+		return email === repeat ? null : { emailsDontMatch: true };
+	}
 
-  passwordValidator(control: AbstractControl): ValidationErrors | null {
-    const value = control.value;
-    if (!value) return null;
+	passwordValidator(control: AbstractControl): ValidationErrors | null {
+		const value = control.value;
+		if (!value) return null;
 
-    const hasUppercase = /[A-Z]/.test(value);
-    const hasNumberOrSymbol = /[\d\W]/.test(value);
+		const hasUppercase = /[A-Z]/.test(value);
+		const hasNumberOrSymbol = /[\d\W]/.test(value);
 
-    if (hasUppercase && hasNumberOrSymbol) {
-      return null;
-    }
+		if (hasUppercase && hasNumberOrSymbol) {
+			return null;
+		}
 
-    return { passwordWeak: true };
-  }
+		return { passwordWeak: true };
+	}
 
-  passwordsMatchValidator(group: AbstractControl): ValidationErrors | null {
-    const pass = group.get('password')?.value;
-    const repeat = group.get('repeatPassword')?.value;
-    return pass === repeat ? null : { passwordsDontMatch: true };
-  }
+	passwordsMatchValidator(group: AbstractControl): ValidationErrors | null {
+		const pass = group.get("password")?.value;
+		const repeat = group.get("repeatPassword")?.value;
+		return pass === repeat ? null : { passwordsDontMatch: true };
+	}
 
-  submit() {
-    if (this.form.valid) {
-      console.log('✅ Registro válido:');
-    } else {
-      console.log('❌ Registro inválido:');
-      this.form.markAllAsTouched();
-    }
-  }
+	submit() {
+		if (this.form.valid) {
+			console.log("✅ Registro válido:");
 
-  isInvalid(control: string): boolean {
-    const c = this.form.get(control);
-    return !!(c?.invalid && (c.dirty || c.touched));
-  }
+			this.store.dispatch(new Register(this.form.value));
 
-  goToLogin() {
-    this.router.navigate(['/login']);
-  }
+			this.actions.pipe(ofActionSuccessful(Register), takeUntil(this._destroyed)).subscribe(() => {
+				this.toastService.showSuccess("Cliente creado correctamente");
+				this.form.reset();
+				this.router.navigate(["/login"]);
+			});
+		} else {
+			console.log("❌ Registro inválido:");
+			this.form.markAllAsTouched();
+		}
+	}
 
-  registerWithGoogle() {
-    console.log('🔐 Registrarse con Google');
-  }
+	isInvalid(control: string): boolean {
+		const c = this.form.get(control);
+		return !!(c?.invalid && (c.dirty || c.touched));
+	}
+
+	goToLogin() {
+		this.router.navigate(["/login"]);
+	}
+
+	registerWithGoogle() {
+		console.log("🔐 Registrarse con Google");
+	}
 }
